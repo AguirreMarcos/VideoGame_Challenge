171e82bbde96bda51e976e08d5425a6ce324c980
AguirreMarcos <AguirreMarcos@users.noreply.github.com>
2021-05-03 21:15:32 +0200

    first commit

---
+++ src/main/java/es/marcosaguirre/videogame/SpringbootVideogameRental1Application.java
@@ -0,0 +1,13 @@
+package es.marcosaguirre.videogame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVideogameRental1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootVideogameRental1Application.class, args);
+	}
+
+}
